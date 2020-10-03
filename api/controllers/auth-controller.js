a9340913d3311594850c8129e3260bafdbdd2ba1
TQP0403 <fantasy.tqp@gmail.com>
2020-10-03 22:44:26 +0700

    hash password

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -1,54 +1,47 @@
 const User = require('./../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+function getToken(user, res, next) {
+  jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_KEY,
+    {
+      expiresIn: '1h',
+    },
+    (err, token) => {
+      if (err) next(err);
+      console.log('token: ' + token);
+      res.json({
+        status: 'success',
+        message: 'Authenticated',
+        token: token,
+      });
+    }
+  );
+}
 
 class AuthController {
   signUp(req, res, next) {
-    new User(req.body.data)
-      .save()
-      .then((user) =>
-        jwt.sign(
-          { id: user.id, email: user.email },
-          process.env.JWT_KEY,
-          {
-            expiresIn: '1h',
-          },
-          (err, token) => {
-            if (err) next(err);
-            console.log('token: ' + token);
-            res.json({
-              status: 'success',
-              message: 'Authenticated',
-              token: token,
-            });
-          }
-        )
-      )
+    let data = req.body.data;
+    bcrypt
+      .hash(data.password, saltRounds)
+      .then((hash) => {
+        data.password = hash;
+        return new User(data).save();
+      })
+      .then((user) => getToken(user, res, next))
       .catch(next);
   }
 
   signIn(req, res, next) {
     User.findOne({ email: req.body.email })
       .then((user) => {
-        if (user) {
-          if (req.body.password === user.password) {
-            jwt.sign(
-              { id: user.id, email: user.email },
-              process.env.JWT_KEY,
-              {
-                expiresIn: '1h',
-              },
-              (err, token) => {
-                if (err) next(err);
-                console.log('token: ' + token);
-                res.json({
-                  status: 'success',
-                  message: 'Authenticated',
-                  token: token,
-                });
-              }
-            );
-          } else next(new Error('Password is incorrect'));
-        } else next(new Error('Email is not exist'));
+        if (!user) throw new Error('Email is not exist');
+        if (!bcrypt.compareSync(req.body.password, user.password))
+          throw new Error('Password is incorrect');
+        getToken(user, res, next);
       })
       .catch(next);
   }

b5e925173e77f7a05db54924d583d68b398be306
TQP0403 <fantasy.tqp@gmail.com>
2020-10-05 20:40:29 +0700

    add socket.io

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+require('dotenv/config');
+
 const router = require('./api/routes/router');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const io = require('socket.io')(http);
 const express = require('express');
 const app = express();
 
-require('dotenv/config');
-
-const hostName = process.env.HOST;
+const ipAddress = require('ip').address();
+const hostName = process.env.HOSTNAME;
 const port = process.env.PORT;
 
 const mongodb = require('./api/services/mongo-db');
@@ -23,6 +25,21 @@ app.use(bodyParser.json());
 
 router.setRoute(app); // route app
 
-app.listen(port, hostName, () =>
-  console.log('Server listening on port ' + port)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.broadcast.emit('hi');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+app.listen(port, () =>
+  console.log('Server listening on: http://' + ipAddress + ':' + port)
 );
